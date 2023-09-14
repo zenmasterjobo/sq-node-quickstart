bdb0fa55b5255c7116a24ccc9e761d614c3c19da
Jordan Bergero <jbergero@squareup.com>
2023-09-14 11:01:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Get Started
+
+1. run `npm i`
+2. copy `.env.example` to new file `.env` and add your square access token
+3. run `node index.js`
---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, Environment, ApiError } = require("square");
+require('dotenv').config()
+
+const client = new Client({
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+  environment: Environment.Sandbox,
+});
+
+const { locationsApi } = client;
+
+async function getLocations() {
+  try {
+    let listLocationsResponse = await locationsApi.listLocations();
+
+    let locations = listLocationsResponse.result.locations;
+
+    locations.forEach(function (location) {
+      console.log(
+        location.id + ": " +
+          location.name +", " +
+          location.address.addressLine1 + ", " +
+          location.address.locality
+      );
+    });
+  } catch (error) {
+    if (error instanceof ApiError) {
+      error.result.errors.forEach(function (e) {
+        console.log(e.category);
+        console.log(e.code);
+        console.log(e.detail);
+      });
+    } else {
+      console.log("Unexpected error occurred: ", error);
+    }
+  }
+};
+
+getLocations();
